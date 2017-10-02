ca25d390c658f5484ba1d14769bc1cd01ecf2a26
Esvethlynna Pangelinan <esveth@aol.com>
2017-10-01 19:50:06 -0700

    add itemTextView in ItemsActivity to display the item entered by the user

--- app/src/main/java/com/epicodus/shoppinglist/ItemsActivity.java
+++ app/src/main/java/com/epicodus/shoppinglist/ItemsActivity.java
@@ -1,13 +1,20 @@
 package com.epicodus.shoppinglist;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ItemsActivity extends AppCompatActivity {
+    private TextView mItemTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_items);
+        mItemTextView = (TextView) findViewById(R.id.itemTextView);
+        Intent intent = getIntent();
+        String item = intent.getStringExtra("item");
+        mItemTextView.setText("Here are all the items: " + item);
     }
 }
--- app/src/main/java/com/epicodus/shoppinglist/MainActivity.java
+++ app/src/main/java/com/epicodus/shoppinglist/MainActivity.java
@@ -3,14 +3,12 @@ package com.epicodus.shoppinglist;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 
 public class MainActivity extends AppCompatActivity {
-    public static final String TAG = MainActivity.class.getSimpleName();
     private Button mFindItemsButton;
     private EditText mItemEditText;
 
@@ -19,15 +17,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mItemEditText = (EditText) findViewById(R.id.itemEditText);
-
         mFindItemsButton = (Button) findViewById(R.id.findItemsButton);
 
         mFindItemsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String item = mItemEditText.getText().toString();
-                Log.d(TAG, item);
                 Intent intent = new Intent (MainActivity.this, ItemsActivity.class);
+                intent.putExtra("item", item);
                 startActivity(intent);
             }
         });
