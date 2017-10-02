172ae01a6b59bcdbc4f1d39eba3df567dfb4ffed
Esvethlynna Pangelinan <esveth@aol.com>
2017-10-01 21:00:19 -0700

    apply custom font to appNameTextView

--- app/src/main/java/com/epicodus/shoppinglist/MainActivity.java
+++ app/src/main/java/com/epicodus/shoppinglist/MainActivity.java
@@ -1,21 +1,29 @@
 package com.epicodus.shoppinglist;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
     private Button mFindItemsButton;
     private EditText mItemEditText;
+    private TextView mAppNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAppNameTextView = (TextView) findViewById(R.id.appNameTextView);
+        Typeface dancingScriptRegular = Typeface.createFromAsset(getAssets(),"fonts/dancingscriptregular.otf");
+        mAppNameTextView.setTypeface(dancingScriptRegular);
+
         mItemEditText = (EditText) findViewById(R.id.itemEditText);
         mFindItemsButton = (Button) findViewById(R.id.findItemsButton);
 
