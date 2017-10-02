d5971033b1bd8ae10a71d190cc456cd260cc77c7
Esvethlynna Pangelinan <esveth@aol.com>
2017-10-01 22:40:28 -0700

    create/implement a ListView and ArrayAdapter in ItemsActivity to display the array of Strings (items)

--- app/src/main/java/com/epicodus/shoppinglist/ItemsActivity.java
+++ app/src/main/java/com/epicodus/shoppinglist/ItemsActivity.java
@@ -3,16 +3,26 @@ package com.epicodus.shoppinglist;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class ItemsActivity extends AppCompatActivity {
     private TextView mItemTextView;
+    private ListView mListView;
+    private String[] items = new String[] {"Plaid Oblong Scarf", "Infinity Scarf", "Scarf w/Fringes", "Border Infinity Scarf", "Cowl Scarf with Fur", "Fancy Shawl Scarf"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_items);
+
+        mListView  = (ListView) findViewById(R.id.listView);
         mItemTextView = (TextView) findViewById(R.id.itemTextView);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, items);
+        mListView.setAdapter(adapter);
+
         Intent intent = getIntent();
         String item = intent.getStringExtra("item");
         mItemTextView.setText("Here are all the items: " + item);
