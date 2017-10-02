3bc9bcf457ac7e13c2710e1f41d6d3a646dfc796
Esvethlynna Pangelinan <esveth@aol.com>
2017-10-01 19:22:22 -0700

    add itemEditText View and gather user input using onClick() method

--- app/src/main/java/com/epicodus/shoppinglist/MainActivity.java
+++ app/src/main/java/com/epicodus/shoppinglist/MainActivity.java
@@ -3,23 +3,30 @@ package com.epicodus.shoppinglist;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.EditText;
+
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
     private Button mFindItemsButton;
+    private EditText mItemEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mItemEditText = (EditText) findViewById(R.id.itemEditText);
 
         mFindItemsButton = (Button) findViewById(R.id.findItemsButton);
 
         mFindItemsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String item = mItemEditText.getText().toString();
+                Log.d(TAG, item);
                 Intent intent = new Intent (MainActivity.this, ItemsActivity.class);
                 startActivity(intent);
             }
