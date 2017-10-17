6c8a7f0068b59f0bfa633c08448691e12042a2c3
Esvethlynna Pangelinan <esveth@aol.com>
2017-10-17 16:04:03 -0700

    implement a ListView and ArrayAdapter to display an array of stores from hard-coded String array.  Add OnItemClickListener in StoresActivity to display a toast of the item in the array when clicked.

--- app/src/main/java/com/epicodus/shoppinglist/StoresActivity.java
+++ app/src/main/java/com/epicodus/shoppinglist/StoresActivity.java
@@ -3,16 +3,38 @@ package com.epicodus.shoppinglist;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class StoresActivity extends AppCompatActivity {
     private TextView mLocationTextView;
+    private ListView mListView;
+    private String[] stores = new String[] {"Renton Walmart Supercenter", "Bellevue Walmart"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stores);
+
+        mListView = (ListView) findViewById(R.id.listView);
         mLocationTextView = (TextView) findViewById(R.id.locationTextView);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, stores);
+        mListView.setAdapter(adapter);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String store = ((TextView)view).getText().toString();
+                Toast.makeText(StoresActivity.this, store, Toast.LENGTH_LONG).show();
+            }
+        });
+
         Intent intent = getIntent();
         String location = intent.getStringExtra("location");
         mLocationTextView.setText("Here are all the stores near: " + location);
